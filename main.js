d77c7b7ae814fc4c158f5e9c871cf95a9a4e6c3e
AndreaPanzerDev <andpanz@libero.it>
2022-11-21 23:55:24 +0100

    finish

--- main.js
+++ main.js
@@ -5,11 +5,15 @@ var app = new Vue({
     },
     methods: {
         printEmail() {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
-                let em = response.data.response;
-                console.log(em);
-                this.prova.push(em);
-            })
+            this.prova = []
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+                    let em = response.data.response;
+                    console.log(em);
+                    this.prova.push(em);
+                })
+            }
+
 
         },
     },
